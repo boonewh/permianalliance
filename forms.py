c1fcde55eef78748173bf05e991a39a88dec19f0
William Boone <138145899+boonewh@users.noreply.github.com>
2024-09-14 11:11:45 -0500

    No message

--- forms.py
+++ forms.py
@@ -1,11 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email
+from wtforms.validators import ValidationError
 
-# Create a form class
+def validate_email(form, field):
+    if field.data.lower() == "kayleighbpsteamship@gmail.com":
+        raise ValidationError("Thank you! We will get back to you soon.")
+
 class ContactForm(FlaskForm):
-  name = StringField("Name",  validators=[DataRequired(message ="Please enter your name.")])
-  email = StringField("Email",validators = [DataRequired(message = "Please enter your email address"),Email()])
-  subject = StringField("Subject",validators = [DataRequired(message = "Please enter a subject.")])
-  message = TextAreaField("Message",validators = [DataRequired(message = "Please enter a message.")])
-  submit = SubmitField("Send") 
+    name = StringField("Name", validators=[DataRequired(message="Please enter your name.")])
+    email = StringField("Email", validators=[DataRequired(message="Please enter your email address"), Email(), validate_email])
+    subject = StringField("What is your current website? (ex: www.name.com or 'none' is also fine.)", validators=[DataRequired(message="Please enter a site or none.")])
+    message = TextAreaField("Briefly describe what you need in your new website. 'Not sure' is a valid response.", validators=[DataRequired(message="Please enter a message. I don't know or I want to talk about it are good responses.")])
+    submit = SubmitField("Send")
